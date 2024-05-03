f9eafc9310b4a49fafb925e978d4929ea0f10b5d
cobyyt <cobyttung@gmail.com>
2024-05-03 14:28:43 +0800

    Second

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
 
 from item.models import Category, Item
 
@@ -30,3 +31,7 @@ def signup(request):
     return render(request, 'core/signup.html', {
         'form': form
     })
+    
+def user_logout(request):
+    logout(request)
+    return redirect('/login/')
